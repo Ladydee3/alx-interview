f6d40c888198596ba4e2175e85f220cc9a2f69d8
Ladydee3 <delisiletwala62@gmail.com>
2024-10-02 10:24:57 -0700

    0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,35 +1,44 @@
-# 0x00. Pascal's Triangle
-
-# 0. Pascal's Triangle
-
-Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
-
-Returns an empty list if n <= 0
-You can assume n will be always an integer
-
-guillaume@ubuntu:~/0x00$ cat 0-main.py
 #!/usr/bin/python3
 """
-0-main
+Create a function def pascal_triangle(n): that returns a list of lists of
+integers representing the Pascal’s triangle of n:
+
+ Returns an empty list if n <= 0
+ You can assume n will be always an integer
+
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-def print_triangle(triangle):
+
+def pascal_triangle(n):
     """
-    Print the triangle
+    Creates a list of lists of integers representing Pascal's triangle
+
+    parameters:
+        n [int]:
+            the number of rows of Pascal's triangle to recreate
+
+    return:
+        [list of lists of ints]:
+            representation of Pascal's triangle
     """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
-
-guillaume@ubuntu:~/0x00$ 
-guillaume@ubuntu:~/0x00$ ./0-main.py
-[1]
-[1,1]
-[1,2,1]
-[1,3,3,1]
-[1,4,6,4,1]
-guillaume@ubuntu:~/0x00$ 
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    triangle = []
+    if n <= 0:
+        return triangle
+    previous = [1]
+    for row_index in range(n):
+        rowlist = []
+        if row_index == 0:
+            rowlist = [1]
+        else:
+            for i in range(row_index + 1):
+                if i == 0:
+                    rowlist.append(0 + previous[i])
+                elif i == (row_index):
+                    rowlist.append(previous[i - 1] + 0)
+                else:
+                    rowlist.append(previous[i - 1] + previous[i])
+        previous = rowlist
+        triangle.append(rowlist)
+    return triangle 
